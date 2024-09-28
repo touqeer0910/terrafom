2ee8ffda108478036e4436899b6d403e93c0692c
touqeer <mohdtouqeerahnmed@gmail.com>
2024-09-28 19:25:16 +0530

    added terraform variable

---
+++ activity1/variable.tf
@@ -0,0 +1,62 @@
+variable "vpc_config" {
+  type = object({
+    cidr_block = string
+    tags       = map(string)
+
+  })
+  default = {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name = "vpc_1"
+    }
+  }
+
+}
+variable "subnets" {
+  type = list(object({
+    cidr_block        = string
+    availability_zone = string
+    vpc_id            = string
+    tags              = map(string)
+  }))
+  default = [{
+    cidr_block        = "10.0.0.0/24"
+    availability_zone = "ap-south-1a"
+    vpc_id            = "aws_vpc.main.id"
+    tags = {
+      Name    = "web-1"
+      Env     = "project"
+      Project = "learning"
+    }
+    }, {
+    cidr_block        = "10.0.1.0/24"
+    availability_zone = "ap-south-1b"
+    vpc_id            = "aws_vpc.main.id"
+    tags = {
+      Name    = "web-2"
+      Env     = "project"
+      Project = "learning"
+    }
+    }, {
+    cidr_block        = "10.0.2.0/24"
+    availability_zone = "ap-south-1a"
+    vpc_id            = "aws_vpc.main.id"
+    tags = {
+      Name    = "db-1"
+      Env     = "project"
+      Project = "learning"
+    }
+    }, {
+    cidr_block        = "10.0.3.0/24"
+    availability_zone = "ap-south-1b"
+    vpc_id            = "aws_vpc.main.id"
+    tags = {
+      Name    = "db-21"
+      Env     = "project"
+      Project = "learning"
+    }
+  }]
+}
+
+
+
